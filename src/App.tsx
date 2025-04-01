9b48d6e5d0ca2ae9be1784aec99943457c10719e
Nicolas <nicolasfpdev@gmail.com>
2025-04-01 16:18:53 -0300

    refactor: remove unnecessary div on page.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 export default function App() {
     return (
-        <div>
+        <>
             <Header />
             <main>
-
+                <Hero />
             </main>
-        </div>
+        </>
     )
 }
