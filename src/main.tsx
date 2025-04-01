9926cb91bc156c3638590c24b5af94e492ec6522
Nicolas <nicolasfpdev@gmail.com>
2025-03-31 21:22:33 -0300

    refactor: adjust on stylesheet imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import "./styles/reset.css"
-import "./styles/global.css"
+import "./styles/global.scss"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
