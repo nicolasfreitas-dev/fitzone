9c32534c86ad2df7cb611f3fee4d393f01c66372
Nicolas <nicolasfpdev@gmail.com>
2025-03-31 21:22:09 -0300

    feat: start create header for navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import Header from "./components/Header";
+
 export default function App() {
-    return <div></div>;
+    return (
+        <div>
+            <Header />
+            <main>
+
+            </main>
+        </div>
+    )
 }
