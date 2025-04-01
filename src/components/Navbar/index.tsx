a9dceb52692f9681413e421e6a09de4e74b4e0ae
Nicolas <nicolasfpdev@gmail.com>
2025-03-31 21:23:51 -0300

    feat: create navbar component to use on header

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,12 @@
+import "./style.scss"
+
+export default function Navbar() {
+    return (
+        <nav className="navbar">
+            <li className="navbar__items">Página inicial</li>
+            <li className="navbar__items">Sobre</li>
+            <li className="navbar__items">Serviços</li>
+            <li className="navbar__items">Contato</li>
+        </nav>
+    )
+};
