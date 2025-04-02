24ea02d37f5e38c941daba293acfd15be37727e6
Nicolas <nicolasfpdev@gmail.com>
2025-04-02 01:36:23 -0300

    refactor: minor adjusts on header style and add navbar element

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,16 @@
-import Navbar from "../Navbar"
 import "./style.scss"
 
 export default function Header() {
     return (
         <header className="header">
             <span className="header__logo">FitZone</span>
-            <Navbar />
-            <button className="header__btn">Matricule-se</button>
+            <nav className="header__navbar">
+                <li className="header__navbar-item">Página inicial</li>
+                <li className="header__navbar-item">Sobre</li>
+                <li className="header__navbar-item">Serviços</li>
+                <li className="header__navbar-item">Contato</li>
+            </nav>
+            <button className="header__btn">Cadastre-se</button>
         </header>
-    )
+    );
 };
