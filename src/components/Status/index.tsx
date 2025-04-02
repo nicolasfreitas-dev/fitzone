d88f1d7725cf0628bdc7f32aa4eda70008b942d1
Nicolas <nicolasfpdev@gmail.com>
2025-04-02 01:33:18 -0300

    feat: initial creation of status bar

---
+++ src/components/Status/index.tsx
@@ -0,0 +1,24 @@
+import "./style.scss"
+
+export default function Status() {
+    return (
+        <section className="status">
+            <div>
+                <span>5+</span>
+                <p>Lorem ipsum</p>
+            </div>
+            <div>
+                <span>5+</span>
+                <p>Lorem ipsum</p>
+            </div>
+            <div>
+                <span>5+</span>
+                <p>Lorem ipsum</p>
+            </div>
+            <div>
+                <span>5+</span>
+                <p>Lorem ipsum</p>
+            </div>
+        </section>
+    )
+};
