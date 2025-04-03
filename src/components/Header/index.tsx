aff0a4477087856758b5e41c7968c472f926a10f
Nicolas <nicolasfpdev@gmail.com>
2025-04-02 23:59:57 -0300

    feat: add responsive sidebar menu when use a small device

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,68 @@
-import "./style.scss"
+import { List, X } from "@phosphor-icons/react";
+import { useResize } from "../../hooks/useResize";
+import "./style.scss";
 
 export default function Header() {
+    const { windowSize } = useResize();
+
+    const handleSidebarVisible = () => {
+        const sidebar = document.querySelector(".header__sidebar");
+
+        sidebar?.classList.toggle("header__sidebar--visible");
+    };
+
     return (
         <header className="header">
             <span className="header__logo">FitZone</span>
-            <nav className="header__navbar">
-                <li className="header__navbar-item">Página inicial</li>
-                <li className="header__navbar-item">Sobre</li>
-                <li className="header__navbar-item">Serviços</li>
-                <li className="header__navbar-item">Contato</li>
-            </nav>
-            <button className="header__btn">Cadastre-se</button>
+            {windowSize >= 768 ? (
+                <nav className="header__navbar">
+                    <li className="header__navbar-item">
+                        <a href="#">Página inicial</a>
+                    </li>
+                    <li className="header__navbar-item">
+                        <a href="#">Sobre</a>
+                    </li>
+                    <li className="header__navbar-item">
+                        <a href="#">Serviços</a>
+                    </li>
+                    <li className="header__navbar-item">
+                        <a href="#">Contato</a>
+                    </li>
+                    <button className="header__btn">Área do cliente</button>
+                </nav>
+            ) : (
+                <>
+                    <List
+                        className="header__menu-toggle"
+                        size={25}
+                        onClick={() => handleSidebarVisible()}
+                    />
+                    <aside className="header__sidebar">
+                        <X
+                            className="header__menu-close"
+                            size={25}
+                            onClick={() => handleSidebarVisible()}
+                        />
+                        <nav className="header__menu">
+                            <li className="header__menu-item">
+                                <a href="#">Página inicial</a>
+                            </li>
+                            <li className="header__menu-item">
+                                <a href="#">Sobre</a>
+                            </li>
+                            <li className="header__menu-item">
+                                <a href="#">Serviços</a>
+                            </li>
+                            <li className="header__menu-item">
+                                <a href="#">Contato</a>
+                            </li>
+                            <button className="header__btn">
+                                Área do cliente
+                            </button>
+                        </nav>
+                    </aside>
+                </>
+            )}
         </header>
     );
-};
+}
