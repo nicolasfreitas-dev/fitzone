de83f8774a911f1c1fec8fb1a328f9d1384d0ccc
Nicolas <nicolasfpdev@gmail.com>
2025-04-03 17:32:43 -0300

    feat: start create ChooseUs component section

---
+++ src/components/ChooseUs/index.tsx
@@ -0,0 +1,15 @@
+import "./style.scss"
+
+export default function ChooseUs() {
+    return (
+        <section className="choose-us">
+            <div>
+                <h2>Porque nos escolher</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores consectetur fugit cum ullam optio iusto quas, quo sequi ut commodi eveniet officiis, ad illum unde molestiae ab est nemo deleniti?</p>
+            </div>
+            <div>
+                
+            </div>
+        </section>
+    )
+};
