352c26395a82d844d7e69efb8e756466981fe526
nicolasfreitas <nicolasfpdev@gmail.com>
2025-04-05 15:58:16 -0300

    feat: add new achievements

--- src/components/Achievements/index.tsx
+++ src/components/Achievements/index.tsx
@@ -4,16 +4,20 @@ export default function Achievements() {
     return (
         <section className="achievements">
             <div className="achievements__item">
-                <span className="achievements__item-number">5+</span>
+                <span className="achievements__item-number">6+</span>
                 <p className="achievements__item-text">Anos de serviço</p>
             </div>
             <div className="achievements__item">
-                <span className="achievements__item-number">15+</span>
-                <p className="achievements__item-text">Treinadores certificados</p>
+                <span className="achievements__item-number">10+</span>
+                <p className="achievements__item-text">Profissionais</p>
             </div>
             <div className="achievements__item">
                 <span className="achievements__item-number">731+</span>
                 <p className="achievements__item-text">Usuários satisfeitos</p>
+            </div>
+            <div className="achievements__item">
+                <span className="achievements__item-number">5</span>
+                <p className="achievements__item-text">Unidades</p>
             </div>
         </section>
     )
