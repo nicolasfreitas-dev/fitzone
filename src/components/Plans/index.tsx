9dc6580ecaf60e5d9901f64b49abdb3bc9d26497
Nicolas <nicolasfpdev@gmail.com>
2025-04-10 17:41:51 -0300

    feat: adjust on responsive layout that was using arbitrary values and add interactivity on plans button

--- src/components/Plans/index.tsx
+++ src/components/Plans/index.tsx
@@ -1,34 +1,50 @@
+import { useState } from "react"
 import CardPlan from "./components/CardPlan"
 import "./style.scss"
 
 export default function Plans() {
+    const [activePlan, setActivePlan] = useState("monthly")
+
     return (
         <section className="plans">
             <div className="plans__header">
                 <h2 className="plans__header-title">Planos</h2>
                 <div className="plans__header-btn">
-                    <button className="plans__header-btn--monthly">Mensal</button>
-                    <button className="plans__header-btn--yearly">Anual</button>
+                    <button
+                        className={`plans__header-btn--monthly ${
+                            activePlan === "monthly" ? "active" : ""
+                        }`}
+                        onClick={() => setActivePlan("monthly")}
+                    >
+                        Mensal
+                    </button>
+                    <button
+                        className={`plans__header-btn--yearly ${
+                            activePlan === "yearly" ? "active" : ""
+                        }`}
+                        onClick={() => setActivePlan("yearly")}
+                    >
+                        Anual
+                    </button>
                 </div>
             </div>
             <div className="plans__card">
-                <CardPlan 
+                <CardPlan
                     planName="Plano Básico"
                     planDescription="Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore nulla, culpa facilis consectetur cupiditate fugiat dolorum excepturi rem dolore maiores?"
                     planValue="99,90"
-                    />
-                <CardPlan 
+                />
+                <CardPlan
                     planName="Plano Premium+"
                     planDescription="Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore nulla, culpa facilis consectetur cupiditate fugiat dolorum excepturi rem dolore maiores?"
                     planValue="149,90"
-                    />
-                <CardPlan 
+                />
+                <CardPlan
                     planName="Plano Premium"
                     planDescription="Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore nulla, culpa facilis consectetur cupiditate fugiat dolorum excepturi rem dolore maiores?"
                     planValue="119,90"
-                    />
-                    
+                />
             </div>
         </section>
-    )
+    );
 };
