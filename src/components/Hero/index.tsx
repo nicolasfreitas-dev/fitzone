a9a88382329728e0ad25a8fefc7980aea431f8ff
Nicolas <nicolasfpdev@gmail.com>
2025-04-11 02:57:38 -0300

    refactor: change hero sub text

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -7,15 +7,25 @@ export default function Hero() {
             <div className="hero__banner">
                 <h1 className="hero__banner-title">Eleve seus treinos</h1>
                 <p className="hero__banner-text">
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                    Sunt, voluptatibus est. Voluptates, dignissimos minus.
-                    Eveniet!
+                    Transforme seu corpo e sua mente com nossa metodologia
+                    exclusiva e profissionais especializados.
                 </p>
-                <button className="hero__banner-btn"><a href="#plans">Ver planos</a></button>
+                <button className="hero__banner-btn">
+                    <a href="#plans">Ver planos</a>
+                </button>
                 <div className="hero__banner-social">
-                    <TwitterLogo className="hero__banner-social-item" size={25} />
-                    <FacebookLogo className="hero__banner-social-item" size={25} />
-                    <InstagramLogo className="hero__banner-social-item" size={25} />
+                    <TwitterLogo
+                        className="hero__banner-social-item"
+                        size={25}
+                    />
+                    <FacebookLogo
+                        className="hero__banner-social-item"
+                        size={25}
+                    />
+                    <InstagramLogo
+                        className="hero__banner-social-item"
+                        size={25}
+                    />
                 </div>
             </div>
         </section>
