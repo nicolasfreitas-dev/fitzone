554a236c5728e7789a0cb7c8edfbdf5527daf9b4
Nicolas <nicolasfpdev@gmail.com>
2025-04-11 16:41:37 -0300

    feat: create contact section

---
+++ src/components/Contact/index.tsx
@@ -0,0 +1,20 @@
+import contactImg from "../../assets/contact.webp"
+import "./style.scss"
+
+export default function Contact() {
+    return (
+        <section className="contact" id="contact">
+            <img
+                className="contact__img"
+                src={contactImg}
+                alt="Imagem de fundo da seção de contato"
+                loading="lazy"
+            />
+            <div className="contact__overlay"></div>
+            <div className="contact__text">
+                <h2 className="contact__text-title">Entre em contato</h2>
+                <h3 className="contact__text-number">(85) 98454-9854</h3>
+            </div>
+        </section>
+    );
+};
