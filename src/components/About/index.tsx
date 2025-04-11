527ba1b59719a90feddc46737f36616f15b260b1
Nicolas <nicolasfpdev@gmail.com>
2025-04-11 02:59:36 -0300

    refactor: change lorem text

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -6,9 +6,25 @@ export default function About() {
         <section className="about" id="about">
             <h2 className="about__title">Sobre nós</h2>
             <div className="about__banner">
-                <p className="about__banner-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed illo tempore voluptatem maiores optio saepe harum rem eveniet voluptate rerum. Omnis quia aliquid magni alias iste possimus reiciendis incidunt quos. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias optio aspernatur quod impedit facere cum exercitationem. Provident veritatis ullam repellendus ex aliquam quis excepturi? Quo expedita dolores officia voluptatum. Eum. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae debitis, commodi aut ullam ut delectus quae aliquam! A itaque accusantium inventore quam omnis delectus porro earum ipsa, reprehenderit facilis assumenda.</p>
-                <img className="about__banner-img" src={aboutImg} alt="fitzone trainer and student" loading="lazy" />
+                <p className="about__banner-text">
+                    Desde 2018, a FitZone tem sido mais que um espaço
+                    para exercícios - somos uma comunidade dedicada à
+                    transformação completa de nossos membros. Com uma equipe de
+                    profissionais certificados, equipamentos modernos e
+                    programas personalizados, criamos um ambiente acolhedor onde
+                    pessoas de todas as idades e níveis de condicionamento
+                    físico encontram suporte para alcançar seus objetivos de
+                    saúde e bem-estar. Acreditamos que fitness vai além da
+                    estética, e nosso compromisso é ajudar você a conquistar sua
+                    melhor versão em um ambiente inclusivo e motivador.
+                </p>
+                <img
+                    className="about__banner-img"
+                    src={aboutImg}
+                    alt="fitzone trainer and student"
+                    loading="lazy"
+                />
             </div>
         </section>
-    )
+    );
 };
