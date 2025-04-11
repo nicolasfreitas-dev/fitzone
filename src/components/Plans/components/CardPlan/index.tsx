085d6ef394e087fea99b9d3a98bf810659b7d636
Nicolas <nicolasfpdev@gmail.com>
2025-04-11 02:54:17 -0300

    feat: add interactivity based on what plan is passed to the card

--- src/components/Plans/components/CardPlan/index.tsx
+++ src/components/Plans/components/CardPlan/index.tsx
@@ -1,37 +1,83 @@
+import { CheckCircle } from "@phosphor-icons/react"
 import "./style.scss"
-import {  } from "@phosphor-icons/react"
 
 type CardPlansProps = {
     planName: string,
     planDescription: string,
     planValue: string,
+    fidelity: string
 }
 
-export default function CardPlan({ planName, planDescription, planValue }: CardPlansProps) {
+export default function CardPlan({ planName, planDescription, planValue, fidelity }: CardPlansProps) {
+    let isPremiumPlus;
+
+    if (planName === "Plano Premium Plus") {
+        isPremiumPlus = true;
+    } else {
+        isPremiumPlus = false;
+    }
+
+    const benefits = [
+        "Área de musculação e aeróbicos.",
+        "Fitzone App (treinos online)",
+        "Acesso ilimitado a +100 academias.",
+        "Leve 5 amigos por mês para treinar com você.",
+        "Cadeira de massagem.",
+        "Acompanhamento nutricional.",
+        "Acesso a uma modalidade esportiva grátis"
+    ];
+
+    const basicBenefitsNotIncluded =
+        planName === "Plano Básico"
+            ? [
+                  "Acesso ilimitado a +100 academias.",
+                  "Leve 5 amigos por mês para treinar com você.",
+                  "Cadeira de massagem.",
+                  "Acompanhamento nutricional.",
+                  "Acesso a uma modalidade esportiva grátis",
+              ]
+            : [];
+
+    const premiumBenefitsNotIncluded =
+        planName === "Plano Premium"
+            ? [
+                  "Acompanhamento nutricional.",
+                  "Acesso a uma modalidade esportiva grátis",
+              ]
+            : [];
+
+            console.log(`[${planName}]`);
+
     return (
-        <div className="cardPlan">
+        <div
+            className={`cardPlan ${
+                isPremiumPlus ? "cardPlan--premiumPlus" : ""
+            }`}
+        >
             <h3 className="cardPlan__name">{planName}</h3>
-            <span className="cardPlan__pricing">R$ {planValue}</span>
+            <p className="cardPlan__pricing">
+                R$ {planValue} <span>/ Mês</span>
+            </p>
+            <p className={`${planName !== "Plano Premium" ? "cardPlan__fidelity" : "cardPlan__no-fidelity"}`}>{fidelity}</p>
             <p className="cardPlan__description">{planDescription}</p>
             <ul className="cardPlan__benefits">
-                <li className="cardPlan__benefits-item">
-                    Lorem ipsum dolor sit amet, consectetur.
-                </li>
-                <li className="cardPlan__benefits-item">
-                    Lorem ipsum dolor sit amet, consectetur.
-                </li>
-                <li className="cardPlan__benefits-item">
-                    Lorem ipsum dolor sit amet, consectetur.
-                </li>
-                <li className="cardPlan__benefits-item">
-                    Lorem ipsum dolor sit amet, consectetur.
-                </li>
-                <li className="cardPlan__benefits-item">
-                    Lorem ipsum dolor sit amet, consectetur.
-                </li>
-                <li className="cardPlan__benefits-item">
-                    Lorem ipsum dolor sit amet, consectetur.
-                </li>
+                {benefits.map((benefit) => (
+                    <li
+                        key={benefit}
+                        className={`cardPlan__benefits-item ${
+                            basicBenefitsNotIncluded.includes(benefit)
+                                ? "benefit--excluded"
+                                : ""
+                        } ${
+                            premiumBenefitsNotIncluded.includes(benefit)
+                                ? "benefit--excluded"
+                                : ""
+                        }`}
+                    >
+                        <CheckCircle size={25} />
+                        <p>{benefit}</p>
+                    </li>
+                ))}
             </ul>
             <button className="cardPlan__button">Contratar agora</button>
         </div>
