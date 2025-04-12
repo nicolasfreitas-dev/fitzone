a0ce98e9eccdcbf79ef77cd7089ea109ca824497
nicolasfreitas <nicolasfpdev@gmail.com>
2025-04-12 09:36:41 -0300

    refactor: change background image of hero to a img html tag for more performance

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,5 +1,6 @@
 import { FacebookLogo, InstagramLogo, TwitterLogo } from "@phosphor-icons/react"
-import { useSmoothScroll } from "../../hooks/useSmoothScroll";
+import { useSmoothScroll } from "../../hooks/useSmoothScroll"
+import heroImage from "../../assets/hero-img.webp"
 import "./style.scss"
 
 export default function Hero() {
@@ -7,13 +8,27 @@ export default function Hero() {
 
     return (
         <section className="hero">
+            <img
+                src={heroImage}
+                alt="Background da academia"
+                className="hero__background-img"
+                loading="eager"
+                fetchPriority="high"
+                width="1600"
+                height="900"
+            />
+            <div className="hero__overlay"></div>
             <div className="hero__banner">
                 <h1 className="hero__banner-title">Eleve seus treinos</h1>
                 <p className="hero__banner-text">
                     Transforme seu corpo e sua mente com nossa metodologia
                     exclusiva e profissionais especializados.
                 </p>
-                <a className="hero__banner-btn" href="#plans" onClick={(e) => handleScroll(e, "plans")}>
+                <a
+                    className="hero__banner-btn"
+                    href="#plans"
+                    onClick={(e) => handleScroll(e, "plans")}
+                >
                     Ver planos
                 </a>
                 <div className="hero__banner-social">
